4b41169da3564ae1a43c269962f40367e5b073bf
hs_note001 <hs_note001@DESKTOP-B9K8AA7>
2017-07-06 16:08:48 +0900

    d

---
+++ finalProject/src/main/java/com/sp/giupNotice/Notice.java
@@ -0,0 +1,66 @@
+package com.sp.giupNotice;
+
+
+public class Notice {
+	private int listNum, noti_Num, noti_Top; 
+	private String noti_Title, noti_Content, noti_Created;
+	private int noti_Hidden, noti_Count;
+	private String admin_Id;
+	
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public int getNoti_Num() {
+		return noti_Num;
+	}
+	public void setNoti_Num(int noti_Num) {
+		this.noti_Num = noti_Num;
+	}
+	public int getNoti_Top() {
+		return noti_Top;
+	}
+	public void setNoti_Top(int noti_Top) {
+		this.noti_Top = noti_Top;
+	}
+	public String getNoti_Title() {
+		return noti_Title;
+	}
+	public void setNoti_Title(String noti_Title) {
+		this.noti_Title = noti_Title;
+	}
+	public String getNoti_Content() {
+		return noti_Content;
+	}
+	public void setNoti_Content(String noti_Content) {
+		this.noti_Content = noti_Content;
+	}
+	public String getNoti_Created() {
+		return noti_Created;
+	}
+	public void setNoti_Created(String noti_Created) {
+		this.noti_Created = noti_Created;
+	}
+	public int getNoti_Hidden() {
+		return noti_Hidden;
+	}
+	public void setNoti_Hidden(int noti_Hidden) {
+		this.noti_Hidden = noti_Hidden;
+	}
+	public int getNoti_Count() {
+		return noti_Count;
+	}
+	public void setNoti_Count(int noti_Count) {
+		this.noti_Count = noti_Count;
+	}
+	public String getAdmin_Id() {
+		return admin_Id;
+	}
+	public void setAdmin_Id(String admin_Id) {
+		this.admin_Id = admin_Id;
+	}
+	
+	
+}

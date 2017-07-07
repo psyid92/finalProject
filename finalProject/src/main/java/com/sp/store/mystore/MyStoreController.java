a4b1be1374447fb3afa788833d2cdfcc79f1b96c
chaejungi <chaejungi@chaejungi>
2017-07-07 16:25:37 +0900

    c채준기

--- finalProject/src/main/java/com/sp/store/mystore/MyStoreController.java
+++ finalProject/src/main/java/com/sp/store/mystore/MyStoreController.java
@@ -1,12 +1,20 @@
 package com.sp.store.mystore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("mystore.myStoreController")
 public class MyStoreController {
+	@Autowired
+	private MyStoreService service;
 	
 	// 메뉴1
 	@RequestMapping(value = "/store/mystore", method = RequestMethod.GET)
@@ -31,6 +39,85 @@ public class MyStoreController {
 		model.addAttribute("subMenu", "3");
 		return ".store4.menu1.menu.list";
 	}
-
 	
+	@RequestMapping(value="/store/insertMenuCT", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> insertMenuCT(MyStore dto)throws Exception{
+		String state = "true";
+		int result = service.insertMenuCT(dto);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("menuct_Num", result);
+		model.put("state", state);
+		return model;
+	}
+	
+	@RequestMapping(value="/store/insertMainMenu", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> insertMainMenu(MyStore dto)throws Exception{
+		String state = "true";
+		int result = service.insertMainMenu(dto);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("mainmenu_Num", result);
+		model.put("state", state);
+		return model;
+	}
+	
+	@RequestMapping(value="/store/insertsubMenu", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> insertsubMenu(MyStore dto)throws Exception{
+		String state = "true";
+		int result = service.insertSubMenu(dto);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("submenu_Num", result);
+		model.put("state", state);
+		return model;
+	}
+	
+	@RequestMapping(value="/store/deleteMenuCT", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> deleteMenuCT(@RequestParam int menuct_Num)throws Exception{
+		String state = "true";
+		int result = service.deleteMenuCT(menuct_Num);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("state", state);
+		return model;
+	}
+	
+	@RequestMapping(value="/store/deleteMainMenu", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> deleteMainMenu(@RequestParam int mainmenu_Num)throws Exception{
+		String state = "true";
+		int result = service.deleteMainMenu(mainmenu_Num);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("state", state);
+		return model;
+	}
+	
+	@RequestMapping(value="/store/deleteSubMenu", method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> deleteSubMenu(@RequestParam int submenu_Num)throws Exception{
+		String state = "true";
+		int result = service.deleteSubMenu(submenu_Num);
+		if(result == 0)
+			state = "false";
+		
+		Map<String , Object> model = new HashMap<>();
+		model.put("state", state);
+		return model;
+	}
 }
--- finalProject/src/main/java/com/sp/store/mystore/MyStoreService.java
+++ finalProject/src/main/java/com/sp/store/mystore/MyStoreService.java
@@ -1,5 +1,11 @@
 package com.sp.store.mystore;
 
 public interface MyStoreService {
-
+	public int insertMenuCT(MyStore dto) throws Exception;
+	public int insertMainMenu(MyStore dto) throws Exception;
+	public int insertSubMenu(MyStore dto) throws Exception;
+	
+	public int deleteMenuCT(int menuct_Num) throws Exception;
+	public int deleteMainMenu(int mainmenu_Num) throws Exception;
+	public int deleteSubMenu(int submenu_Num) throws Exception;
 }

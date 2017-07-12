5f65763237027964331b58daa1b046d81ce8fbbc
user1 <user1@user1-PC>
2017-07-12 11:01:08 +0900

    as

--- finalProject/src/main/java/com/sp/jumun/JumunService.java
+++ finalProject/src/main/java/com/sp/jumun/JumunService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface JumunService {
+	public List<Jumun> listCategory() throws Exception;
 	public List<Jumun> listGiup (Map<String, Object> map) throws Exception;
 	public List<Jumun> readMenuCategory (int g1_Num) throws Exception;
 	public List<Jumun> readMainMenu (int menuct_Num) throws Exception;

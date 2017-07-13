0d4ac11a31ff5e17f4dbce6e656d5fe0a3c04504
hunxhun <mirihunter@gmail.com>
2017-07-13 14:19:16 +0900

    store stat

---
+++ finalProject/src/main/java/com/sp/store/mystore/MyStoreStatController.java
@@ -0,0 +1,24 @@
+package com.sp.store.mystore;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("mystore.myStoreController")
+public class MyStoreStatController {
+	@Autowired
+	private MyStoreStatService service;
+	
+	// 메뉴1
+		@RequestMapping(value = "/store/mystore", method = RequestMethod.GET)
+		public String mystoreForm(Model model, HttpSession session) {
+
+			model.addAttribute("mainMenu", "0");
+			model.addAttribute("subMenu", "1");
+			return ".store4.menu1.mystore.list";
+		}
+}
